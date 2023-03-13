a25ec543808bdfb6290e7c9d81c2d3c454a43874
changach-3000 <sharon.changach@student.moringaschool.com>
2023-03-13 14:37:16 +0300

    Done with lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name="Ralph"){
+    cats.push(name);   
+}
+function destructivelyPrependCat(name="Bob"){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+
+}
+function appendCat(name="Broom"){
+    const cats2 = cats.slice();
+    cats2.push(name)
+    return cats2
+}
+function prependCat(name="Arnold"){
+    const cats3 = cats.slice();
+    cats3.unshift(name)
+    return cats3
+
+}
+function removeLastCat(){
+    const cats4 = cats.slice();
+    cats4.pop();
+    return cats4
+}
+function removeFirstCat(){
+    const cats5 = cats.slice();
+    cats5.shift();
+    return cats5
+}
